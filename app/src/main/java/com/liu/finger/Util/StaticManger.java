5cbee579a0722e64cd69c4e12912d1f10af8f053
xiongzhuo <15388953585@163.com>
2017-07-05 17:11:08 +0800

    指纹解锁。

---
+++ app/src/main/java/com/liu/finger/Util/StaticManger.java
@@ -0,0 +1,8 @@
+package com.liu.finger.Util;
+
+/**
+ * Created by paytend_liu on 2016/5/17.
+ */
+public class StaticManger {
+    public final static String TAG = "finger_log";
+}
---
+++ app/src/main/java/com/liu/finger/Util/UtilTools.java
@@ -0,0 +1,12 @@
+package com.liu.finger.Util;
+
+import android.util.Log;
+
+/**
+ * Created by paytend_liu on 2016/5/17.
+ */
+public class UtilTools {
+    public static void Log(String tag, String msg) {
+        Log.d(tag, msg);
+    }
+}
